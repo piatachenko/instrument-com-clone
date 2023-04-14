0f5fdf503606d5df941423d6498df5b8b00b10a3
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-04-14 19:58:07 +0200

    Add carousel of images

---
+++ src/components/ImagesCarousel.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React, { CSSProperties, useEffect, useState } from "react";
+
+interface CSSCustomProps extends CSSProperties {
+  "--delay": string;
+}
+
+interface ImagesCarouselProps {
+  images: string[];
+  duration: number;
+  delay?: number;
+}
+
+export default function ImagesCarousel({
+  images,
+  duration,
+  delay,
+}: ImagesCarouselProps) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      setImageIndex((imageIndex) => (imageIndex + 1) % images.length);
+    }, duration);
+    return () => clearInterval(interval);
+  }, [imageIndex]);
+
+  return (
+    <>
+      <ul className="relative min-h-[45rem] w-full">
+        {Array.from(images.keys()).map((i) => (
+          <li className="absolute bottom-1/2 right-1/2 w-full translate-x-1/2 translate-y-1/2">
+            <Image
+              src={images[i] ?? ""}
+              className={`transition-all delay-[--delay] duration-[.4s] ${
+                imageIndex === i
+                  ? "translate-y-0 opacity-100"
+                  : "-translate-y-5 opacity-0"
+              }`}
+              style={{ "--delay": `${delay}ms` } as CSSCustomProps}
+              alt=""
+              width={2000}
+              height={2000}
+            ></Image>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}

b445cd0e08eeee94e78a1b7e106f6dde0653b4f2
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-04-25 12:10:00 +0200

    Fix logo import

--- src/layouts.tsx/HeaderNavbar.tsx
+++ src/layouts.tsx/HeaderNavbar.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
-import logo from "~/assets/logo.svg";
+import Logo from "~/assets/Logo";
 import LinkSelectElement from "~/components/LinkSelectElement";
 
 export default function HeaderNavbar() {
@@ -34,16 +33,16 @@ export default function HeaderNavbar() {
   return (
     <>
       <header
-        className={`fixed flex w-full items-center justify-between bg-black py-12 pl-14 pr-5 transition-all duration-700 ${
+        className={`fixed flex w-full items-center justify-between bg-black py-12 px-[4%] transition-all duration-700 z-10 max-md:hidden ${
           isFixed ? "" : "-translate-y-[8rem]"
         }`}
       >
         <div>
           <Link href="/">
-            <Image src={logo} alt="Instrument Logo" width={110} />
+            <Logo className="w-[110px]" />
           </Link>
         </div>
-        <nav>
+        <nav className="-mr-9">
           <LinkSelectElement
             text="What We Do"
             href="https://www.instrument.com/what-we-do"

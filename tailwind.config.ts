2fd43618310b7080838c8ce6122cfbf95879ce15
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-04-28 12:01:00 +0200

    Fix tailwind's responsive breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'xs': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+    },
     extend: {},
   },
   plugins: [],
